91b325e6481de0415b87026f4b38822d0886ef8d
e <sishengqikuo_xx@163.com>
2018-10-10 18:14:56 +0800

    2018-10-10   单词字典树

---
+++ src/main/java/com/li/cayenne/dictionary/tree/Main.java
@@ -0,0 +1,25 @@
+package com.li.cayenne.dictionary.tree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DictionaryTree dt = new DictionaryTree();
+
+        dt.add("interest");
+        dt.add("interesting");
+        dt.add("interested");
+        dt.add("inside");
+        dt.add("insert");
+        dt.add("apple");
+        dt.add("inter");
+        dt.add("interesting");
+
+        dt.print();
+
+        boolean isFind = dt.find("inside");
+        System.out.println("find inside : " + isFind);
+
+        int count = dt.count("inter");
+        System.out.println("count prefix inter : " + count);
+    }
+}
